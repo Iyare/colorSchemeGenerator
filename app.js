f419e074b818a3cd6a181de453e1efb09282e568
Iyare Usen <iyare.usen@gmail.com>
2025-07-10 12:50:02 +0100

    fixed non-responsiveness to change of select values and color

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 (function () {
     
-    const submitBtn = document.querySelector('#get-color');
     const form = document.querySelector('form');
-    const formData = new FormData(form, submitBtn);
     const colorDisplay = document.querySelector('#color-container');
         
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         getColorScheme();
     })
-    
-   
+       
     const getColorScheme = async () => {
-        const colorValue = formData.get('color').slice(1);
-        const colorScheme = formData.get('scheme');
+        const colorValue = document.querySelector('#color-picker').value.slice(1);
+        const colorScheme = document.querySelector('#color-schemes').value;
         const colorNumbers = 6
+        colorDisplay.innerHTML = ``;
         
         const api = `https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${colorScheme}&count=${colorNumbers}&format=json`;
         
         const response = await fetch(api);
         const data = await response.json();
         renderColor(data);
-        // console.log(data.colors[0].hex.value)
+      
     }
 
     function renderColor(apiData) {
